edbbe5ed722c21d3c62eb621f1ba6d446c73e738
Muhammad Abdul Azizul Hakim <113509874+azihakim@users.noreply.github.com>
2025-01-31 08:41:41 +0700

    cutiizin

--- resources/views/cutiizin/create.blade.php
+++ resources/views/cutiizin/create.blade.php
@@ -35,12 +35,12 @@
 								type="text" class="form-control">
 						</div>
 					</div>
-					<div class="form-group row" style="display: none">
+					<div class="form-group row">
 						<label class="col-sm-2 col-form-label">Jenis Pengajuan</label>
 						<div class="col-sm-10">
 							<select required class="form-control" name="jenis">
 								<option value="">Pilih Pengajuan</option>
-								<option value="Cuti" selected>Cuti</option>
+								<option value="Cuti">Cuti</option>
 								<option value="Izin">Izin</option>
 							</select>
 						</div>
